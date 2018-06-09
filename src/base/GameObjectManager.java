d33cd695dd72dad2dbca612a043a10ef4d268427
Ahihi <hoangnguyenbkict>
2018-06-09 20:40:01 +0700

    Uplate

--- src/base/GameObjectManager.java
+++ src/base/GameObjectManager.java
@@ -5,6 +5,7 @@ import game.enemy.Enemy;
 import game.player.Player;
 import game.player.PlayerMove;
 import physic.BoxCollider;
+import physic.PhysicBody;
 import renderer.PolygonRenderer;
 
 import javax.swing.*;
@@ -52,22 +53,47 @@ public class GameObjectManager {
                 .orElse(null);
     }
 
-    public Enemy checkCollision(Bullet bullet){
-        return (Enemy) this.list
+//    public Enemy checkCollision(Bullet bullet){
+//        return (Enemy) this.list
+//                .stream()
+//                .filter(gameObject -> gameObject.isAlive)
+//                .filter(gameObject -> gameObject instanceof Enemy)
+//                .filter(gameObject -> {
+//                    BoxCollider other = ((Enemy) gameObject).boxCollider;
+//                    return bullet.boxCollider.checkCoxCollider(other);
+//                })
+//                .findFirst()
+//                .orElse(null);
+//    }
+
+    public <T extends GameObject> T checkCollision(BoxCollider boxCollider, Class<T> cls){
+        return (T) this.list
                 .stream()
-                .filter(gameObject -> gameObject.isAlive)
-                .filter(gameObject -> gameObject instanceof Enemy)
+                .filter(gameObject -> gameObject.isAlive )
+                .filter(gameObject -> cls.isInstance(gameObject))
+                .filter(gameObject -> gameObject instanceof PhysicBody)
                 .filter(gameObject -> {
-                    BoxCollider other = ((Enemy) gameObject).boxCollider;
-                    return bullet.boxCollider.checkCoxCollider(other);
+                    BoxCollider other = ((PhysicBody) gameObject).getBoxCollider();
+                    return boxCollider.checkCoxCollider(other);
                 })
                 .findFirst()
                 .orElse(null);
+    }
+
+    public <T extends GameObject> T recycle(Class<T> cls) {
+        T object = (T) this.list.stream().filter(gameObject1 -> !gameObject1.isAlive)
+                .filter(gameObject -> cls.isInstance(gameObject))
+                .findFirst()
+                .orElse(null);
+        if(object != null){
+            object.isAlive = true;
+        }else{ try {
+            object = cls.newInstance();
+            this.add(object);
+        } catch (InstantiationException | IllegalAccessException e) {
+            e.printStackTrace();
+          }
         }
-    public Player checkCollision1(Enemy enemy){
-        return (Player) this.list.stream().filter(gameObject -> gameObject.isAlive).filter(gameObject -> gameObject instanceof Player).filter(gameObject ->{
-            BoxCollider other = ((Player) gameObject).boxCollider;
-            return enemy.boxCollider.checkCoxCollider(other);
-        }).findFirst().orElse(null);
+    return object;
     }
 }
--- src/game/player/Player.java
+++ src/game/player/Player.java
@@ -1,21 +1,19 @@
 package game.player;
 
 import base.GameObject;
-import base.GameObjectManager;
 import base.Vector2D;
-import game.enemy.Enemy;
-import physic.BoxCollider;
+import physic.RunHitObject;
 import renderer.PolygonRenderer;
 
 import java.awt.*;
 
 public class Player extends GameObject {
-
     public PlayerMove playerMove;
     public PlayerShoot playerShoot;
-    public BoxCollider boxCollider;
+    private RunHitObject runHitObject;
 
     public Player() {
+        this.position = new Vector2D();
         this.renderer = new PolygonRenderer(
                 Color.RED,
                 new Vector2D(),
@@ -24,7 +22,6 @@ public class Player extends GameObject {
         );
         this.playerMove = new PlayerMove();
         this.playerShoot = new PlayerShoot();
-        this.boxCollider = new BoxCollider(20,20);
     }
 
     @Override
@@ -32,14 +29,6 @@ public class Player extends GameObject {
         super.run();
         this.playerMove.run(this);
         this.playerShoot.run(this);
-        this.boxCollider.position.set(this.position.x - 10, this.position.y -10);
-        Player player = GameObjectManager.instance.checkCollision1(this);
-        if(player != null){
-            player.isAlive = false;
-            this.isAlive = false;
-            System.out.println("hit");
-        }
         ((PolygonRenderer) this.renderer).angle = this.playerMove.angle;
     }
 }
-
